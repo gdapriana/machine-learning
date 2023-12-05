444a90916e46e40eba2a278c2cea9bed6ff06a29
gedeapriana <gedeapriana36@gmail.com>
2023-12-06 01:30:39 +0800

    Add files via upload

---
+++ TensorFlow-Developer-Certificate-Exam-Simulation/Simulation A/Problem_A1.py
@@ -0,0 +1,53 @@
+# =================================================================================
+# PROBLEM A1
+#
+# Given two arrays, train a neural network model to match the X to the Y.
+# Predict the model with new values of X [-2.0, 10.0]
+# We provide the model prediction, do not change the code.
+#
+# The test infrastructure expects a trained model that accepts
+# an input shape of [1].
+# Do not use lambda layers in your model.
+#
+# Please be aware that this is a linear model.
+# We will test your model with values in a range as defined in the array to make sure your model is linear.
+#
+# Desired loss (MSE) < 1e-4
+# =================================================================================
+
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+
+def solution_A1():
+    # DO NOT CHANGE THIS CODE
+    X = np.array([-4.0, -3.0, -2.0, -1.0, 0.0, 1.0,
+                  2.0, 3.0, 4.0, 5.0], dtype=float)
+    Y = np.array([5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0,
+                  12.0, 13.0, 14.0, ], dtype=float)
+
+    # YOUR CODE HERE
+    model = keras.Sequential([
+        keras.layers.Dense(1, input_shape=[1])
+    ])
+
+    class Callback(keras.callbacks.Callback):
+        def on_epoch_end(self, epoch, logs=None):
+            if logs.get('loss') < 1e-4:
+                self.model.stop_training = True
+
+    model.compile(optimizer='sgd', loss='mean_squared_error')
+    model.fit(X, Y, epochs=1000, callbacks=[Callback()])
+
+    print(model.predict([-2.0, 10.0]))
+    return model
+
+
+# The code below is to save your model as a .h5 file.
+# It will be saved automatically in your Submission folder.
+if __name__ == '__main__':
+    # DO NOT CHANGE THIS CODE
+    model = solution_A1()
+    model.save("model_A1.h5")
